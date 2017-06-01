3c18da1cac895cb4226d419797d405a990ef340f
Unknown <Diamondrubix@gmail.com>
2017-06-01 16:14:50 -0500

    Auto stash before merge of "master" and "origin/master"

--- phoneRoutes.js
+++ phoneRoutes.js
@@ -7,8 +7,43 @@ app.post('/phone', function (req, res) {
     res.send('POST request to the homepage')
 })
 
-app.post('/phoneLogin', function (req,res) {
-    console.log(req.body)
+app.post('/oldphoneLogin', function (req,res) {
+    console.log(req.body.username)
+    console.log(req.body.password)
     res.send("logged in threw phoneloge in")
 
 })
+
+
+app.post('/', function (req, res) {
+    //console.log("HERE");
+    dbManager.getOne({username:req.body.username},"users",function(e,err){
+
+        if(e){
+
+            if(sha1(req.body.password)===e.password){
+
+                if(e.status!="active"){
+                    res.send("invalid login")
+                    res.end();
+                    return;
+                }
+
+                req.session.username=req.body.username;
+
+                db.collection("users").update(
+                    {username: req.body.username },
+                    {$set: {uid: req.sessionID } }
+                );
+
+
+            }
+        }
+        res.writeHead(302, {
+            'Location': "/login?invalid=y"
+        });
+        res.end();
+    });
+
+
+});
